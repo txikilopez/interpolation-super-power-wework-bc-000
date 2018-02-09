84360e6eb207a33d41cd54dd859fbe6db053b911
Antonio Troncoso <antonio.troncoso@wework.com>
2018-02-09 00:59:35 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(x)
-  puts("R: #{x[0]}")
-  puts("O: #{x[1]}")
-  puts("Y: #{x[2]}")
-  puts("G: #{x[3]}")
-  puts("B: #{x[4]}")
-  puts("I: #{x[5]}")
-  puts("V: #{x[6]}")
+  puts("R: #{x[0]}, ")
+  puts("O: #{x[1]}, ")
+  puts("Y: #{x[2]}, ")
+  puts("G: #{x[3]}, ")
+  puts("B: #{x[4]}, ")
+  puts("I: #{x[5]}, ")
+  puts("V: #{x[6]}, ")
 end
